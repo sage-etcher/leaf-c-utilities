35dc7c7ab86ae999d06caace30cd2a69b0234672
U-IT-DESKTOP\admin <sage-etcher@Github>
2023-07-27 15:46:22 -0400

    gitignore was ignoring sourcefiles, for now it will be empty

---
+++ scripts/nav-bar.js
@@ -0,0 +1,9 @@
+
+// swap navbar to mobile view when screen size gets small
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.sidenav');
+	var instances = M.Sidenav.init(elems, edge='right');
+});
+
+
+
